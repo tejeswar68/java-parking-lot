0e62bde92260bceadcb4b526e6c12d1f03c88fa8
Saket03-P <saket10r.palarapu@gmail.com>
2022-07-05 21:37:48 +0530

    first change

--- first.java
+++ first.java
@@ -3,5 +3,6 @@ public class first
     public static void main(String args[])
     {
         System.out.println("Hello");
+        System.out.println("Java Parking Lot Management System");
     }
 }
