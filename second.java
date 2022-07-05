75eec7a9c572c5d6ccd91fb653361e061e1d12d9
sreya1911 <sreyatirumalaraju@gmail.com>
2022-07-05 22:05:21 +0530

    Bye added

--- second.java
+++ second.java
@@ -3,5 +3,7 @@ public class second
     public static void main(String args[])
     {
         System.out.println("Yo !!");
+        System.out.println("Sup? ");
+        System.out.println("Byeee");
     }
 }
